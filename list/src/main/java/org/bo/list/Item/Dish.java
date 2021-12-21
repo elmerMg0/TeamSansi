ed6b58f0041bb39d15a9fec989940009c7f07cf9
Felipe Sanchez <felipe.sanchez2028@gmail.com>
2021-12-21 15:56:41 -0400

    Class Dish and Drink created and extend from Item

---
+++ list/src/main/java/org/bo/list/Item/Dish.java
@@ -0,0 +1,7 @@
+package org.bo.list.Item;
+
+public class Dish extends Item {
+    public Dish(String idItem, String name, String description, double price) {
+        super(idItem, name, description, price);
+    }
+}
---
+++ list/src/main/java/org/bo/list/Item/Drink.java
@@ -0,0 +1,7 @@
+package org.bo.list.Item;
+
+public class Drink extends Item {
+    public Drink(String idItem, String name, String description, double price) {
+        super(idItem, name, description, price);
+    }
+}
