9f92dc542590ac33545095de9d854e9ed8b88414
elmerMg <elmermendoza121@gmail.com>
2021-12-22 15:18:33 -0400

    Class Menu created

---
+++ list/src/main/java/org/bo/list/Item/Menu.java
@@ -0,0 +1,18 @@
+package org.bo.list.Item;
+
+import java.util.ArrayList;
+
+public class Menu {
+    private ArrayList<Item> avaibleItems;
+    private ArrayList<Item> orderDishes;
+
+    public Menu(){
+        avaibleItems = new ArrayList<>();
+        orderDishes = new ArrayList<>();
+    }
+
+    public void addItem(Item item){
+        avaibleItems.add(item);
+    }
+}
+
