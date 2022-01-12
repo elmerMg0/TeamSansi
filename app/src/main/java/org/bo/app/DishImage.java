a6e952f3728ffa0f4a6db25033b98b4698cf5a8f
Felipe Sanchez <felipe.sanchez2028@gmail.com>
2022-01-12 12:10:48 -0400

    Modified database and added images for all dishes

--- app/src/main/java/org/bo/app/DishImage.java
+++ app/src/main/java/org/bo/app/DishImage.java
@@ -1,5 +1,6 @@
 package org.bo.app;
 
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
@@ -9,13 +10,21 @@ import java.io.InputStream;
 
 public class DishImage extends VBox {
 
-    public DishImage() {
-        ImageView imageView;
-        String path = new File("src/main/java/org/bo/app/img/pique.jpeg").toURI().toString();
-        Image image = new Image(path);
-        imageView = new ImageView(image);
+    private String dishName;
+    private String pathImage;
 
-        this.getChildren().addAll(imageView);
+    public DishImage(String dishName, String pathImage) {
+        this.dishName = dishName;
+        this.pathImage = pathImage;
+
+        Label name = new Label(dishName);
+        String path = new File(pathImage).toURI().toString();
+        Image image = new Image(path);
+        ImageView imageView = new ImageView(image);
+
+        imageView.setFitHeight(160);
+        imageView.setFitWidth(280);
+        this.getChildren().addAll(name, imageView);
     }
 
 }
--- list/src/main/java/org/bo/list/Item/Dish.java
+++ list/src/main/java/org/bo/list/Item/Dish.java
@@ -2,13 +2,13 @@ package org.bo.list.Item;
 
 public class Dish extends ItemDTO {
 
-    public Dish(int idItem, String name, String description, double price) {
-        super(idItem, name, description, price);
+    public Dish(int idItem, String name, String description, double price, String pathImage) {
+        super(idItem, name, description, price, pathImage);
         super.isDish = true;
     }
 
-    public Dish(String name, String description, double price) {
-        super(name, description, price);
+    public Dish(String name, String description, double price, String pathImage) {
+        super(name, description, price, pathImage);
         super.isDish = true;
     }
 }
