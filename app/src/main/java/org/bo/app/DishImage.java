587bbafd5f123727447b125b60454318f509ce5a
Felipe Sanchez <felipe.sanchez2028@gmail.com>
2022-01-18 15:39:03 -0400

    Menucontext added for each DishImage

--- app/src/main/java/org/bo/app/DishImage.java
+++ app/src/main/java/org/bo/app/DishImage.java
@@ -1,7 +1,9 @@
 package org.bo.app;
 
 import javafx.scene.Cursor;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
@@ -25,12 +27,22 @@ public class DishImage extends VBox {
         Image image = new Image(path);
         ImageView imageView = new ImageView(image);
 
+        ContextMenu contextMenu = new ContextMenu();
+
+        MenuItem view = new MenuItem("Ver");
+        MenuItem edit = new MenuItem("Editar");
+        MenuItem delete = new MenuItem("Eliminar");
+
+        contextMenu.getItems().addAll(view, edit, delete);
+        this.setOnContextMenuRequested(event -> contextMenu.show(this, event.getScreenX(), event.getScreenY()));
+
         imageView.setFitHeight(280);
         imageView.setFitWidth(280);
         imageView.setCursor(Cursor.HAND);
         this.getChildren().addAll(name, imageView);
         this.setOnMouseClicked(event -> {
             if (event.getButton() == MouseButton.PRIMARY) {
+                contextMenu.hide();
                 clickWindow();
             }
         });
