8cb6832fb1debd54d6e9b574cb70e539c840e1b7
Felipe Sanchez <felipe.sanchez2028@gmail.com>
2022-02-03 21:51:16 -0400

    Encrypt testing

--- list/src/main/java/org/bo/list/generator/Test.java
+++ list/src/main/java/org/bo/list/generator/Test.java
@@ -6,26 +6,68 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.AlgorithmParameters;
+import java.security.GeneralSecurityException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
 
 public class Test {
 
-    public static void main(String[] args) throws IOException {
-        PDDocument document = new PDDocument();
-        PDPage page = new PDPage(PDRectangle.A4);
-        document.addPage(page);
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        String password = "password";
+        byte[] salt = "12345678".getBytes();
+        int iterationCount = 40000;
+        int keyLength = 128;
+        SecretKeySpec key = createSecretKey(password.toCharArray(), salt, iterationCount, keyLength);
 
-        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        String originalPassword = password;
+        System.out.println("Original password: " + originalPassword);
+        String encryptedPassword = encrypt(originalPassword, key);
+        System.out.println("Encrypted password: " + encryptedPassword);
+        String decryptedPassword = decrypt(encryptedPassword, key);
+        System.out.println("Decrypted password: " + decryptedPassword);
+    }
 
-        contentStream.beginText();
-        contentStream.setFont(PDType1Font.HELVETICA, 32);
-        contentStream.newLineAtOffset(20, page.getMediaBox().getHeight() - 100);
-        contentStream.showText("Hello World");
-        contentStream.endText();
+    public static SecretKeySpec createSecretKey(char[] password, byte[] salt, int iterationCount, int keyLength) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchAlgorithmException, InvalidKeySpecException {
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+        PBEKeySpec keySpec = new PBEKeySpec(password, salt, iterationCount, keyLength);
+        SecretKey keyTmp = keyFactory.generateSecret(keySpec);
+        return new SecretKeySpec(keyTmp.getEncoded(), "AES");
+    }
 
-        contentStream.close();
+    public static String encrypt(String dataToEncrypt, SecretKeySpec key) throws GeneralSecurityException, UnsupportedEncodingException {
+        Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        pbeCipher.init(Cipher.ENCRYPT_MODE, key);
+        AlgorithmParameters parameters = pbeCipher.getParameters();
+        IvParameterSpec ivParameterSpec = parameters.getParameterSpec(IvParameterSpec.class);
+        byte[] cryptoText = pbeCipher.doFinal(dataToEncrypt.getBytes("UTF-8"));
+        byte[] iv = ivParameterSpec.getIV();
+        return base64Encode(iv) + ":" + base64Encode(cryptoText);
+    }
 
-        document.save("app/src/main/java/org/bo/app/pdf/Hello world.pdf");
+    private static String base64Encode(byte[] bytes) {
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+    public static String decrypt(String string, SecretKeySpec key) throws GeneralSecurityException, IOException {
+        String iv = string.split(":")[0];
+        String property = string.split(":")[1];
+        Cipher pbeCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        pbeCipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(base64Decode(iv)));
+        return new String(pbeCipher.doFinal(base64Decode(property)), "UTF-8");
+    }
+
+    private static byte[] base64Decode(String property) throws IOException {
+        return Base64.getDecoder().decode(property);
     }
 
 }
